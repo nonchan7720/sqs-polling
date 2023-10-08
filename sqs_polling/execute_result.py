06e195e49cdda0ab08b816201b0832cf9582f5a6
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-10-08 13:33:49 +0000

    パッケージの修正

--- sqs_polling/execute_result.py
+++ sqs_polling/execute_result.py
@@ -7,3 +7,18 @@ class ExecuteResult(IntEnum):
     Retry = auto()
     Reject = auto()
     SendDLQ = auto()
+
+    def __str__(self) -> str:
+        match self:
+            case ExecuteResult.Nil:
+                return "Nil"
+            case ExecuteResult.Deletable:
+                return "Deletable"
+            case ExecuteResult.Retry:
+                return "Retry"
+            case ExecuteResult.Reject:
+                return "Reject"
+            case ExecuteResult.SendDLQ:
+                return "SendDLQ"
+            case _:
+                return "Missing"
